ac95f1279b407caed59664d1adc05e5082e83644
Alesha <121282755+oAleshao@users.noreply.github.com>
2023-12-13 00:38:26 +0300

    Add project files.

---
+++ ClassWork08.12.2023MVCPatter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClassWork08._12._2023MVCPatter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ClassWork08.12.2023MVCPatter/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassWork08._12._2023MVCPatter.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; } 
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Director { get; set; }
+        public string? Country { get; set; }
+        public string? Ganre { get; set; }
+        public int Year { get; set; }
+        public string? Cast { get; set; }
+        public string? Poster { get; set; }
+
+        public Movie() { }
+    }
+}
